38dce4fca0c876aa97faa4b69862c267cbe837cc
Tushar Chaurasia <77012813+Dark-CodeX@users.noreply.github.com>
2025-03-05 21:34:15 +0530

    UPDATE: 1. driver code for the `qubit` class

--- qubitverse/simulator/simulator/simulator.cc
+++ qubitverse/simulator/simulator/simulator.cc
@@ -1,42 +1,29 @@
-/**
- * @file simulator.cc
- * @license This file is licensed under the GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007. You may obtain a copy of this license at https://www.gnu.org/licenses/gpl-3.0.en.html.
- * @author Tushar Chaurasia (Dark-CodeX)
- */
-
-#include "../gates/gates.hh"
 #include <iostream>
+#include <cmath>
+#include "../gates/gates.hh"
 
 int main()
 {
-    simulator::qubit<2> qubits;
+    simulator::qubit<3> qsys;
+    qsys.apply_hadamard(0);
+    qsys.apply_hadamard(1);
+    qsys.apply_hadamard(2);
 
-    std::cout << "Initial 2-qubit state:" << std::endl;
-    const auto &s1 = qubits.get_qubits();
-    for (std::size_t i = 0; i < qubits.get_size(); ++i)
+    const auto &s1 = qsys.get_qubits();
+    for (std::size_t i = 0; i < qsys.get_size(); i++)
     {
-        std::cout << "State[" << i << "] = " << s1[i] << std::endl;
+        std::cout << "State [" << i << "] = " << s1[i] << "\n";
     }
 
-    qubits.apply_hadamard(0);
+    simulator::qubit<3>::complex q1[2], q2[2], q3[2];
+    qsys.get_nth_qubit(q1, 0);
+    qsys.get_nth_qubit(q2, 1);
+    qsys.get_nth_qubit(q3, 2);
 
-    std::cout << "\nAfter applying HADAMARD (H-Gate) on qubit 0:" << std::endl;
-    const auto &s2 = qubits.get_qubits();
-    for (std::size_t i = 0; i < qubits.get_size(); ++i)
-    {
-        std::cout << "State[" << i << "] = " << s2[i] << std::endl;
-    }
+    std::cout << "QUBIT 0: " << q1[0] << ", " << q1[1] << "\n";
+    std::cout << "QUBIT 1: " << q2[0] << ", " << q2[1] << "\n";
+    std::cout << "QUBIT 2: " << q3[0] << ", " << q3[1] << "\n";
 
-    qubits.apply_cnot(0, 1);
-
-    std::cout << "\nAfter applying CNOT on qubit 0 as control-qubit and 1 as target-qubit:" << std::endl;
-    const auto &s3 = qubits.get_qubits();
-    for (std::size_t i = 0; i < qubits.get_size(); ++i)
-    {
-        std::cout << "State[" << i << "] = " << s3[i] << std::endl;
-    }
-
-    std::cout << "\nMeasured Result: " << qubits.measure() << std::endl;
-
+    std::cout << "Measured Result: " << qsys.measure() << std::endl;
     return 0;
 }
