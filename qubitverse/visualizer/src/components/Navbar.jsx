aec179f627b48785bc606c827e3262cd2c2cd41a
Tushar Chaurasia <77012813+Dark-CodeX@users.noreply.github.com>
2025-03-31 05:18:18 +0530

    UPDATE: 1. made sure that user does not select html text

--- qubitverse/visualizer/src/components/Navbar.jsx
+++ qubitverse/visualizer/src/components/Navbar.jsx
@@ -13,11 +13,12 @@ const Navbar = () => {
 
     return (
         <nav className="flex items-center justify-between px-6 py-4 bg-gray-100 dark:bg-gray-800 shadow">
-            <h1 className="text-3xl font-bold text-gray-800 dark:text-white">QubitVerse</h1>
+            <h1 className="text-3xl font-bold text-gray-800 dark:text-white" style={{userSelect: "none"}}>QubitVerse</h1>
             <div className="flex items-center space-x-5">
                 <button
                     onClick={() => setDarkTheme((prev) => !prev)}
                     className="px-4 py-2 bg-gray-200 dark:bg-gray-700 text-gray-800 dark:text-gray-200 rounded focus:outline-none"
+                    style={{userSelect: "none"}}
                 >
                     {darkTheme ? "Light Mode" : "Dark Mode"}
                 </button>
@@ -26,8 +27,9 @@ const Navbar = () => {
                     target="_blank"
                     rel="noopener noreferrer"
                     className="text-gray-800 dark:text-gray-200 hover:text-blue-500 dark:hover:text-blue-400 transition-colors"
+                    style={{userSelect: "none"}}
                 >
-                    <img src="/Octicons-mark-github.svg" width={38} height={38} style={{ filter: "invert(1)"}}></img>
+                    <img src="/Octicons-mark-github.svg" width={38} height={38} style={{ filter: "invert(1)", userSelect: "none"}}></img>
                 </a>
             </div>
         </nav>
