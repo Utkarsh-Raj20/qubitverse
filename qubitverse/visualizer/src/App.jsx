a2cdb4a1479d8b097fe0a37413463224e671b686
Tushar Chaurasia <77012813+Dark-CodeX@users.noreply.github.com>
2025-04-06 03:54:26 +0530

    UPDATE: 1. prompt the user for number of Qubits

--- qubitverse/visualizer/src/App.jsx
+++ qubitverse/visualizer/src/App.jsx
@@ -1,12 +1,20 @@
+import React, { useState, useRef } from 'react';
 import QuantumCircuit from "./components/QuantumCircuit";
 import "./App.css";
 import Navbar from "./components/Navbar";
+import NQubitInput from "./components/NQubitInput";
 
 function App() {
+  const [nQubits, setNQubits] = useState(null);
+
   return (
     <>
       <Navbar />
-      <QuantumCircuit />
+      {nQubits === null ? (
+        <NQubitInput numQubits={nQubits} setQubits={setNQubits} />
+      ) : (
+        <QuantumCircuit numQubits={nQubits} setNumQubits={setNQubits} />
+      )}
     </>
   );
 }
