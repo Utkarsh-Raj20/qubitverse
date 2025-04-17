f83bfc5b59c4861ff90df01c53605bfa1506d394
Tushar Chaurasia <77012813+Dark-CodeX@users.noreply.github.com>
2025-04-17 14:05:03 +0530

    UPDATE: 1. removed dark mode button 2. updated repo link

--- qubitverse/visualizer/src/components/Navbar.jsx
+++ qubitverse/visualizer/src/components/Navbar.jsx
@@ -30,13 +30,13 @@ const Navbar = () => {
                     <code>QubitVerse</code>
                 </div></h1>
             <div className="flex items-center space-x-5">
-                <button
+                {/* <button
                     onClick={() => setDarkTheme((prev) => !prev)}
                     className="px-4 py-2 bg-gray-200 dark:bg-gray-700 text-gray-800 dark:text-gray-200 rounded focus:outline-none"
                     style={{ userSelect: "none" }}
                 >
                     {darkTheme ? "Light Mode" : "Dark Mode"}
-                </button>
+                </button> */}
                 <button
                     onClick={() => location.reload()}
                     className="px-4 py-2 bg-gray-200 dark:bg-gray-700 text-gray-800 dark:text-gray-200 rounded focus:outline-none"
@@ -45,7 +45,7 @@ const Navbar = () => {
                     Clear Circuit
                 </button>
                 <a
-                    href="https://github.com/Dark-CodeX/qubitverse"
+                    href="https://github.com/QubitStorm-HackStacy/QubitStorm"
                     target="_blank"
                     rel="noopener noreferrer"
                     className="text-gray-800 dark:text-gray-200 hover:text-blue-500 dark:hover:text-blue-400 transition-colors"
